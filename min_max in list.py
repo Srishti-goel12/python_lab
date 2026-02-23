4152fe0f149eaa9925a98d5e303e63c8b67f0071
Srishti-goel12 <srishti.goel.mca.2025@miet.ac.in>
2026-02-23 10:37:25 +0530

    Update min_max in list.py

--- min_max in list.py
+++ min_max in list.py
@@ -7,4 +7,10 @@ minimum = min(numbers)
 
 print("List:", numbers)
 print("Maximum value:", maximum)
+
 print("Minimum value:", minimum)
+
+#output
+List: [10, 25, 5, 40, 15]
+Maximum value: 40
+Minimum value: 5
