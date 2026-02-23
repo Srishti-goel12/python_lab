2d82b617af8e336d2da576fa37e58fd17d35b779
Srishti-goel12 <srishti.goel.mca.2025@miet.ac.in>
2026-02-23 10:35:27 +0530

    Add files via upload

---
+++ min_max in list.py
@@ -0,0 +1,10 @@
+# Create a list
+numbers = [10, 25, 5, 40, 15]
+
+# Find max and min
+maximum = max(numbers)
+minimum = min(numbers)
+
+print("List:", numbers)
+print("Maximum value:", maximum)
+print("Minimum value:", minimum)
---
+++ sum_calulation_using_append.py
@@ -0,0 +1,17 @@
+# Empty list banayi
+numbers = []
+
+# User se 5 numbers lena
+for i in range(5):
+    num = int(input("Enter number: "))
+    numbers.append(num)   # Number list me add ho raha hai
+
+# Sum calculate karna
+total = sum(numbers)
+
+print("Numbers =", numbers)
+print("Total Sum =", total)
+# append() → har input ko list ke end me add karta hai
+
+# sum(numbers) → list ke sabhi elements ka total nikalta hai
+
